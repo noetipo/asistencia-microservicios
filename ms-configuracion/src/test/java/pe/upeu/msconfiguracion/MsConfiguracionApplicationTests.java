219a0fc5ed5d5d46251ff2b40411f4e3db9df096
noetipo <noe.tipo@gmail.com>
2023-05-09 11:23:55 -0500

    primer commit

---
+++ ms-configuracion/src/test/java/pe/upeu/msconfiguracion/MsConfiguracionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upeu.msconfiguracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfiguracionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
