f14c215cf119546e121b799d742d57ec6dbbd45b
Noe Tipo <noe.tipo@gmail.com>
2023-05-16 14:29:42 -0500

    cambios

---
+++ ms-bff-asistencia-web/src/main/java/pe/upeu/msbffasistenciaweb/MsBffAsistenciaWebApplication.java
@@ -0,0 +1,13 @@
+package pe.upeu.msbffasistenciaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBffAsistenciaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBffAsistenciaWebApplication.class, args);
+    }
+
+}
---
+++ ms-bff-asistencia-web/src/test/java/pe/upeu/msbffasistenciaweb/MsBffAsistenciaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upeu.msbffasistenciaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBffAsistenciaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
