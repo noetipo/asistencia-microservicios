a0a5ecb9577387e3fec4ebeda8735b35b560dbc2
noetipo <noe.tipo@gmail.com>
2023-06-16 04:01:29 -0500

    cambios

---
+++ ms-admin-server/src/test/java/pe/upeu/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upeu.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
